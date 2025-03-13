ff390ebb56440619ccc427115636b816799cca17
frncscjesus <franciscosfjesus@gmail.com>
2025-03-13 00:24:50 +0000

    client

--- code/client/client.c
+++ code/client/client.c
@@ -1,3 +1,26 @@
-int main() {
+#include "client.h"
+
+int main(void){
+/*
+inicializações
+
+*/
+
+
+    while(1){
+        
+    }
+
     return 0;
 }
+
+/*
+    to do next - 2 structs -> cmd stream e datagrama
+*/
+
+/* 
+    estrutura geral do código para um cliente:
+    -> faz as inicializações necessárias quando é criado um novo cliente;
+    -> espera instruções por parte do utilizador (constantemente);
+    -> 
+*/
