27af68d2f1c8301be9fb593da75e386929c6cfd9
frncscjesus <franciscosfjesus@gmail.com>
2025-03-17 00:07:50 +0000

    cmd again
    mistakes fixed;
    2 new functions developed;
    formatting + comments;

--- cmd_fj/comando.c
+++ cmd_fj/comando.c
@@ -16,31 +16,34 @@
 #define DIMPLAY1 3
 #define DIMPLAY2 5
 #define CLINAME "/tmp/CLI"
+#define JMMLOGSD "/tmp/JMMLOGS"    /* nome do registo histórico (socket datagram) */
 #define JMMSERVSD "/tmp/JMMSERVSD" /* nome do servidor de jogo (socket datagram) */
 #define JMMSERVSS "/tmp/JMMSERVSS" /* nome do servidor de jogo (socket stream) */
-#define JMMLOGSD "/tmp/JMMLOGS"    /* nome do registo histórico (socket datagram) */
 
-int sd_stream;
-int sd_datagram;
+
+// VARIÁVEIS GLOBAIS
 int dif;
+
+int sd_stream;                    // file descriptor do socket stream
+socklen_t addrlen_s;
+struct sockaddr_un srv_addr_s;    
+
+int sd_datagram;                  // file descriptor do socket datagrama
+socklen_t tolen_d;
+socklen_t addrlen_d;
+struct sockaddr_un to_d;
+struct sockaddr_un my_addr_d;
+
+
 
 /*-------------------------------------------------------------------------+
 | Function: cmd_sair - termina a aplicacao
 +--------------------------------------------------------------------------*/ 
-void cmd_sair (int argc, char **argv){
-  // falta fechar os sockets aqui
+void cmd_sair (int argc, char **argv){        // POSSO FAZER close(..) ou unlink(..) A COISAS QUE NÃO EXISTEM? OU SEJA SD AINDA NÃO TINHA SIDO CRIADO P.E.
+  close(sd_stream);
+  //unlink(my_addr_d.sun_path)
+  close(sd_datagram);
   exit(0);
-}
-
-
-/*-------------------------------------------------------------------------+
-| Function: cmd_test - apenas como exemplo
-+--------------------------------------------------------------------------*/ 
-void cmd_test (int argc, char** argv){
-  int i;
-
-  for (i = 0; i < argc; i++)
-    printf ("\nargv[%d] = %s", i, argv[i]);
 }
 
 
@@ -53,34 +56,25 @@ void cmd_cnj (int argc, char** argv){
   char buf_d[50];
   char new_game[9];
   
-  struct sockaddr_un srv_addr_s;          // stream
-  socklen_t addrlen_s;
-
-  struct sockaddr_un my_addr_d;          // datagram <- p/ não confundir
-  socklen_t addrlen_d;
-  struct sockaddr_un to_d;
-  socklen_t tolen_d;
-  
-  
   if(argc != 3){    // comando + nome + dificuldade
-    printf("Número de argumentos inválido\n");
-    return;
+    printf("[ERRO] Número de argumentos inválido\n");
+    return;     // volta para a "linha de comandos"
   }
   if(strlen(argv[1]) != 3){     // nome com 3 chars
-    printf("\n\nNome de jogador inválido! Introduzir 3 caracteres.\n\n");
-    return;
+    printf("\n\n[ERRO] Nome inválido! Introduzir no máximo 3 caracteres.\n\n");
+    return;     // volta para a "linha de comandos"
   }else{
     dif = atoi(argv[2]);
     if((dif == 1) || (dif == 2)){     // dificuldade: 1 ou 2
     /*-----------------------criar-socket-stream------------------------*/
       if((sd_stream = socket(AF_UNIX, SOCK_STREAM, 0)) < 0 ){  // tenta criar socket
-        perror("Erro a criar socket. Tentar novamente"); 
-        return;     // retorna para a "linha de comandos"
+        perror("[ERRO] Criação de socket stream falhou. Tentar novamente:\n"); 
+        return;     // volta para a "linha de comandos"
       }
 
       if((sd_stream = socket(AF_UNIX, SOCK_STREAM, 0)) < 0 ){
-        perror("Erro a criar socket. Tentar novamente:\n"); 
-        return;
+        perror("[ERRO] Criação de socket stream falhou. Tentar novamente:\n"); 
+        return;     // volta para a "linha de comandos"
       }
 
       srv_addr_s.sun_family = AF_UNIX;
@@ -89,37 +83,38 @@ void cmd_cnj (int argc, char** argv){
       addrlen_s = sizeof(srv_addr_s.sun_family) + strlen(srv_addr_s.sun_path);
     
       if(connect(sd_stream, (struct sockaddr *)&srv_addr_s, addrlen_s) < 0){
-        perror("Erro no connect. Tentar novamente"); 
-        return;     // retorna para a "linha de comandos"
+        perror("[ERRO] Connect. Tentar novamente:\n"); 
+        return;     // volta para a "linha de comandos"
       }
       
       sprintf(new_game, "%s %s %s", argv[0], argv[1], argv[2]);
-      /*strcpy(new_game, strcat(strcat(strcat(argv[0], " "), strcat(argv[1], " ")), argv[2]));*/
       if((write(sd_stream, new_game, strlen(new_game) + 1) < 0)){
-        perror("Erro no write para o servidor. Tentar novamente:\n");
+        perror("[ERRO] Write para o servidor. Tentar novamente:\n");
+        close(sd_stream);
+        return;     // volta para a "linha de comandos"
       }else{
         while(read(sd_stream, buf_s, sizeof(buf_s)) < 0);
-        printf("cliente recebeu confirmação: %s\n", buf_s);      
+        printf("[INFO] Cliente recebeu confirmação: %s\n", buf_s);      
       }
 
     /*----------------------criar-socket-datagrama----------------------*/
       if((sd_datagram = socket(AF_UNIX, SOCK_DGRAM, 0)) < 0 ){     // tenta criar um socket datagrama
-        perror("Erro a criar socket datagrama. Tentar Novamente:\n");
+        perror("[ERRO] Criação de socket datagrama falhou. Tentar Novamente:\n");
         close(sd_stream);
-        return;
+        return;     // volta para a "linha de comandos"
       }
         
       my_addr_d.sun_family = AF_UNIX;
       memset(my_addr_d.sun_path, 0, sizeof(my_addr_d.sun_path));
       strcpy(my_addr_d.sun_path, CLINAME);
       sprintf(mypid, "%d", getpid());
-      strcat(my_addr_d.sun_path, mypid);
+      strcat(my_addr_d.sun_path, mypid);      // junta o path dos clientes com o pid criando um identificador único
       addrlen_d = sizeof(my_addr_d.sun_family) + strlen(my_addr_d.sun_path);
       
-      if(bind(sd_datagram, (struct sockaddr *)&my_addr_d, addrlen_d) < 0 ){          // 
-        perror("Erro no bind do socket datagrama. Tentar novamente:\n");
+      if(bind(sd_datagram, (struct sockaddr *)&my_addr_d, addrlen_d) < 0 ){
+        perror("[ERRO] Bind do socket datagrama. Tentar novamente:\n");
         close(sd_stream); 
-        return;
+        return;     // volta para a "linha de comandos"
       }
   
       to_d.sun_family = AF_UNIX;
@@ -139,7 +134,7 @@ void cmd_cnj (int argc, char** argv){
       }
       */
     }else{
-      printf("Nível de dificuldade (%d) inválido! Inserir dificuldade: 1 ou 2.\n\n", dif);
+      printf("[ERRO] Nível de dificuldade (%d) inválido! Inserir dificuldade: 1 ou 2.\n\n", dif);
       return;
     }
   }
@@ -147,7 +142,7 @@ void cmd_cnj (int argc, char** argv){
 
 
 /*-------------------------------------------------------------------------+
-| Function: cmd_jg - jogada
+| Function: cmd_jg - fazer jogada
 +--------------------------------------------------------------------------*/
 void cmd_jg (int argc, char** argv){
   int aux;
@@ -157,7 +152,7 @@ void cmd_jg (int argc, char** argv){
 
   if(dif == 1){
     if(strlen(argv[1]) != 3){
-      printf("Repetir jogada! Introduzir 3 letras (de {ABCDE})");
+      printf("[ERRO] Repetir jogada! Introduzir 3 letras (de {ABCDE})");
       return;
     }
 
@@ -165,23 +160,22 @@ void cmd_jg (int argc, char** argv){
     for(aux = 0; aux < DIMPLAY1; aux++){
       xua = dif1_play[aux];
       if((xua != 'A') || (xua != 'B') || (xua != 'C') || (xua != 'D') || (xua != 'E')){
-        printf("Repetir jogada! Introduzir 3 letras (de {ABCDE})");
+        printf("[ERRO] Repetir jogada! Introduzir 3 letras (de {ABCDE})");
         return;
       }
     }
 
     if((write(sd_stream, argv[1], strlen(argv[1]) + 1) < 0)){
-      perror("Erro no write para o servidor. Tentar novamente:\n");
+      perror("[ERRO] Write para o servidor. Tentar novamente:\n");
       return;
     }else{
       while(read(sd_stream, dif1_play, sizeof(dif1_play)) < 0);
-      printf("jogada: %s\n", dif1_play);      
+      printf("[INFO] Jogada: %s\n", dif1_play);      
     }
-
   }
   if(dif == 2){
     if(strlen(argv[1]) != 5){
-      printf("Repetir jogada! Introduzir 3 letras (de {ABCDE})");
+      printf("[ERRO] Repetir jogada! Introduzir 3 letras (de {ABCDE})");
       return;
     }
 
@@ -189,19 +183,18 @@ void cmd_jg (int argc, char** argv){
     for(aux = 0; aux < DIMPLAY1; aux++){
       xua = dif2_play[aux];
       if((xua != 'A') || (xua != 'B') || (xua != 'C') || (xua != 'D') || (xua != 'E') || (xua != 'F') || (xua != 'G') || (xua != 'H')){
-        printf("Repetir jogada! Introduzir 5 letras (de {ABCDEFGH})");
+        printf("[ERRO] Repetir jogada! Introduzir 5 letras (de {ABCDEFGH})");
         return;
       }
     }
 
     if((write(sd_stream, argv[1], strlen(argv[1]) + 1) < 0)){
-      perror("Erro no write para o servidor. Tentar novamente:\n");
+      perror("[ERRO] Write para o servidor. Tentar novamente:\n");
       return;
     }else{
       while(read(sd_stream, dif2_play, sizeof(dif2_play)) < 0);
-      printf("jogada: %s\n", dif2_play);      
+      printf("[INFO] Jogada: %s\n", dif2_play);      
     }
-
   }
 }
 
@@ -210,16 +203,55 @@ void cmd_jg (int argc, char** argv){
 | Function: cmd_clm - consultar limites
 +--------------------------------------------------------------------------*/
 void cmd_clm (int argc, char** argv){
-  // faz pedido por datagrama ao servidor dos limites
-  printf("1");
+  char requested_info[15];
+
+  if(sendto(sd_datagram, argv[0], strlen(argv[0]) + 1, 0, (struct sockaddr *)&to_d, tolen_d) < 0){
+    printf("[ERRO] Envio de pedido ao servidor");
+    return;     // volta para a "linha de comandos"
+  }else{
+    if(recvfrom(sd_datagram, requested_info, sizeof(requested_info), 0, (struct sockaddr *)&to_d, &tolen_d) < 0){
+      printf("[ERRO] Receção de informação do servidor");
+      return;     // volta para a "linha de comandos"                                            
+    }else{
+      printf("[INFO] Informação recebida: %s\n", requested_info);
+    }
+  }
 }
 
 
 /*-------------------------------------------------------------------------+
 | Function: cmd_mlm - mudar limites
 +--------------------------------------------------------------------------*/
-void cmd_mlm (int, char**){
-  printf("1");
+void cmd_mlm (int argc, char** argv){
+  char request[50];
+  char from_serv[50];
+  int jogadas, tempo;
+
+  if(argc == 3){
+    jogadas = atoi(argv[1]);
+    tempo = atoi(argv[2]);
+
+    if((jogadas > 0) || (tempo > 0)){
+      sprintf(request, "%s %s %s", argv[0], argv[1], argv[2]);
+      
+      if(sendto(sd_datagram, request, strlen(request) + 1, 0, (struct sockaddr *)&to_d, tolen_d) < 0){
+        printf("[ERRO] Envio de pedido ao servidor");
+        return;     // volta para a "linha de comandos"
+      }else{                                                                          /* ALGO DO GÉNERO "PEDIDO CONCEBIDO. NOVOS LIMITES: ..." */
+        if(recvfrom(sd_datagram, from_serv, sizeof(from_serv), 0, (struct sockaddr *)&to_d, &tolen_d) < 0){
+          printf("[ERRO] Receção de informação do servidor");
+          return;     // volta para a "linha de comandos"                                            
+        }else{
+          printf("[INFO] Informação recebida: %s\n", from_serv);
+        }
+      }
+
+    }else{
+      printf("[ERRO] Argumento(s) fora dos limites válidos.");
+    }
+  }else{
+    printf("[ERRO] Número de argumentos inválido. Escrever 'sos' para consultar ajuda.\n");
+  }
 }
 
 
@@ -255,6 +287,9 @@ void cmd_tmm (int argc, char** argv){
 }
 
 
+
+/*++++++++++++++++++++++++++++++++_JMMlog_++++++++++++++++++++++++++++++*/
+
 /*-------------------------------------------------------------------------+
 | Function: cmd_ltc - listar classificações
 +--------------------------------------------------------------------------*/
@@ -276,4 +311,15 @@ void cmd_rtc (int argc, char** argv){
 +--------------------------------------------------------------------------*/
 void cmd_trh (int argc, char** argv){
   printf("1");
+}
+
+
+/*-------------------------------------------------------------------------+
+| Function: cmd_test - função de teste: apenas como exemplo
++--------------------------------------------------------------------------*/ 
+void cmd_test (int argc, char** argv){
+  int i;
+
+  for (i = 0; i < argc; i++)
+    printf ("\nargv[%d] = %s", i, argv[i]);
 }
